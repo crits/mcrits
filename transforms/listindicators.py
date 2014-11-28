6a9a40f8ad4960096e45eae42397bdc89c5664f5
Wesley Shields <wxs@atarininja.org>
2014-11-28 16:50:53 -0500

    Make lisindicators use pycrits.

--- transforms/listindicators.py
+++ transforms/listindicators.py
@@ -3,94 +3,61 @@
 # Author: Brian Warehime @nulltr0n
 # 11/14/2014
 
-# Importing various modules
-
 from MaltegoTransform import *
-import requests
-import json
-import pprint
-import re
-import os
-import ConfigParser
+from mcrits_utils import get_crits
 
-# Configuration Parser to grab necessary options.
+def addentities(ioc):
+    global me
 
-def addentities():
-	#Maltego Entities
-	if ioc['type'] == "Address - ipv4-addr":
-		ent = me.addEntity("maltego.IPv4Address",ioc['value'])
-	elif ioc['type'] == "Address - e-mail":
-		ent = me.addEntity("maltego.EmailAddress",ioc['value'])
-	elif ioc['type'] == "URI - Domain Name":
-		ent = me.addEntity("maltego.Domain",ioc['value'])
-	elif ioc['type'] == "URI - URL":
-		ent = me.addEntity("maltego.URL",ioc['value'])
-	elif ioc['type'] == "Network Subnet" or ioc['type'] == "Address - cidr" or ioc['type'] == "Address - ipv4-net":
-		ent = me.addEntity("maltego.Netblock",ioc['value'])
+    #Maltego Entities
+    if ioc['type'] == "Address - ipv4-addr":
+        ent = me.addEntity("maltego.IPv4Address", ioc['value'])
+    elif ioc['type'] == "Address - e-mail":
+        ent = me.addEntity("maltego.EmailAddress", ioc['value'])
+    elif ioc['type'] == "URI - Domain Name":
+        ent = me.addEntity("maltego.Domain", ioc['value'])
+    elif ioc['type'] == "URI - URL":
+        ent = me.addEntity("maltego.URL", ioc['value'])
+    elif ioc['type'] == "Network Subnet" or ioc['type'] == "Address - cidr" or ioc['type'] == "Address - ipv4-net":
+        ent = me.addEntity("maltego.Netblock", ioc['value'])
 
-	#mcrits Specific Entities
-	elif ioc['type'].startswith("Disk"):
-		ent = me.addEntity("mcrits.Disk",ioc['value'])
-	elif ioc['type'].startswith("Library"):
-		ent = me.addEntity("mcrits.Library",ioc['value'])
-	elif ioc['type'].startswith("Win"):
-		ent = me.addEntity("mcrits.Windows",ioc['value'])
-	else:
-		ent = me.addEntity("mcrits.Indicator",ioc['value'])
-	for elem in elems:
-		if ioc[elem] == []:
-			pass
-		else:
-			if len(ioc[elem][0]) == 1:
-				ent.addAdditionalFields(elem, elem,'',ioc[elem])
-			else:
-				ent.addAdditionalFields(elem, elem,'',ioc[elem][0])
-				ent.addAdditionalFields('confidence','confidence','',ioc['confidence']['rating'])
-				ent.addAdditionalFields('impact','impact','',ioc['impact']['rating'])
-				ent.addAdditionalFields('source','source','',ioc['source'][0]['name'])
-				ent.addAdditionalFields('campaign', 'campaign','',campaignname)
+    #mcrits Specific Entities
+    elif ioc['type'].startswith("Disk"):
+        ent = me.addEntity("mcrits.Disk", ioc['value'])
+    elif ioc['type'].startswith("Library"):
+        ent = me.addEntity("mcrits.Library", ioc['value'])
+    elif ioc['type'].startswith("Win"):
+        ent = me.addEntity("mcrits.Windows", ioc['value'])
+    else:
+        ent = me.addEntity("mcrits.Indicator", ioc['value'])
 
-def getLocalConfPath():
-   	pathname = os.path.dirname(sys.argv[0])
-	pathname = os.path.abspath(pathname)
-	pathname = os.path.join(pathname, '..','local', 'mcrits.conf')
-	return os.path.normpath(pathname)
-
-configFile = getLocalConfPath()
-config = ConfigParser.SafeConfigParser()
-config.read(configFile)
-
-username = config.get('info', 'username')
-url = config.get('info', 'url')
-api_key = config.get('info', 'api_key')
-
-# Setting up Maltego entities and getting initial variables.
+    for elem in ['_id', 'bucket_list', 'created', 'modified', 'sectors', 'status']:
+        if ioc[elem] != []:
+            if len(ioc[elem][0]) == 1:
+                ent.addAdditionalFields(elem, elem, '', ioc[elem])
+            else:
+                ent.addAdditionalFields(elem, elem, '', ioc[elem][0])
+                ent.addAdditionalFields('confidence', 'confidence', '', ioc['confidence']['rating'])
+                ent.addAdditionalFields('impact', 'impact', '', ioc['impact']['rating'])
+                ent.addAdditionalFields('source', 'source', '', ioc['source'][0]['name'])
+                ent.addAdditionalFields('campaign', 'campaign', '', campaignname)
 
 me = MaltegoTransform()
 me.parseArguments(sys.argv)
 ioctype = me.getVar("ioctype")
 campaignname = me.getVar("campaign")
 
-# Setting up requests variables from mcrits.conf
+crits = get_crits()
 
-url = url + '/api/v1/indicators/'
-params = {
-'api_key': api_key,
-'username': username,
-'c-campaign.name': campaignname
-}
+params = { 'c-campaign.name': campaignname, 'c-type': ioctype }
 
-elems = ['_id','bucket_list','created','modified','sectors','status']
+for batch in crits.indicators(params=params):
+    for ioc in batch:
+        if ioc['campaign'] == [] and campaignname == "Unknown":
+            ioc['campaign'] = [{'name': campaignname}]
 
-r = requests.get(url, params=params, verify=False)
-j = json.loads(r.text)
-for ioc in j['objects']:
-	if ioc['campaign'] == [] and campaignname == "Unknown" and ioc['type'] == ioctype:
-		addentities()
-	else:
-		for value in ioc['campaign']:
-			if value['name'] == campaignname and ioc['type'] == ioctype:
-				addentities()
-
+        for value in ioc['campaign']:
+            if value['name'] == campaignname:
+                addentities(ioc)
 
 me.returnOutput()
